f46dadbe3d17eaf01ed4b9be867f1db64c724f36
ghostRV <71167770+ghostRV@users.noreply.github.com>
2020-12-09 11:37:43 +0500

    1
    1

---
+++ app/src/main/java/com/deevrv/assigment/DataClass.kt
@@ -0,0 +1,5 @@
+package com.deevrv.assigment
+
+import java.io.Serializable
+
+class DataClass (val text5: String, val text6: String ):Serializable
--- app/src/main/java/com/deevrv/assigment/MainActivity.kt
+++ app/src/main/java/com/deevrv/assigment/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.deevrv.assigment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button1.setOnClickListener(){
+
+            var inputText1: String?
+            var inputText2: String?
+
+            inputText1 = input1.text.toString()
+            inputText2 = input2.text.toString()
+            val redText = DataClass("$inputText1", "$inputText2")
+
+
+            val intent = Intent (this, MainActivity2::class.java )
+            intent.putExtra("next3", redText)
+//            intent.putExtra("next1", "$inputText1")
+//            intent.putExtra("next2", "$inputText2")
+            startActivity(intent)
+        }
+        val myText3 = intent.extras?.getString ("textNew3") ?: return
+        val getText4 = intent.extras?.getString("textNew4")?: return
+
+        text3.text = " Вновь передано: $myText3"
+        text4.text = " Вновь передано: $getText4"
+
     }
 }
 
